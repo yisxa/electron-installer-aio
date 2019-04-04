b07b36a462aa5b684ec5edfee57a2cb984879c3d
Bandhu <yisxaa@gmail.com>
2019-04-04 20:46:53 +0530

    electron-reload configured

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { app, BrowserWindow } from 'electron';
+// autoreload for changes
+require('electron-reload')(__dirname);
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
